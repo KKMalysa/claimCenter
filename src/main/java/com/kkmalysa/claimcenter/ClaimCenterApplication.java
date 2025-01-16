e6c4aab9051db861ccc38ce824282a7d5fefc64e
karol <k.malysa010@gmail.com>
2025-01-16 13:01:33 +0100

    Initial commit

---
+++ src/main/java/com/kkmalysa/claimcenter/ClaimCenterApplication.java
@@ -0,0 +1,13 @@
+package com.kkmalysa.claimcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaimCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClaimCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kkmalysa/claimcenter/ClaimCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkmalysa.claimcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaimCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
